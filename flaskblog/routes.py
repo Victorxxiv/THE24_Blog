a35c2219caf7ba2fade4991c913b497bdbae1c90
Victor <victormwendwa804@gmail.com>
2024-09-03 18:45:10 +0300

    Adding profile pic for account

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -75,4 +75,5 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account')
